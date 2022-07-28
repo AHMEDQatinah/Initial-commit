b224c040879e04d12ea1289aaa2495c13729b07a
AHMEDQatinah <106144237+AHMEDQatinah@users.noreply.github.com>
2022-07-28 18:43:33 +0300

    lnitial commit

--- 1212 .js
+++ 1212 .js
@@ -1,3 +1,29 @@
+document.querySelector(".menu").addEventListener("click", () => {
+  document.querySelectorAll(".target").forEach((item) => {
+    item.classList.toggle("change");
+  });
+});
+
+document.querySelectorAll(".wrapper").forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".target").forEach((item) => {
+      item.classList.remove("change");
+    });
+  });
+});
+
+const videos = document.querySelectorAll(".video");
+
+videos.forEach((video) => {
+  video.addEventListener("mouseover", () => {
+    video.play();
+  });
+  video.addEventListener("mouseout", () => {
+    video.pause();
+  });
+});
+
+
 var images1 = [
   "img/1957 Mercedes 300SL Gullwing/1.jfif",
   "img/1957 Mercedes 300SL Gullwing/2.jpg",
@@ -18,24 +44,6 @@ function changeImages1() {
 }
 changeImages1();
 
-var images2 = [
-  "img/1960s Jaguar E-Type/2.jfif",
-  "img/1960s Jaguar E-Type/3.jfif",
-  "img/1960s Jaguar E-Type/4.jfif",
-  "img/1960s Jaguar E-Type/5.jfif",
-  "img/1960s Jaguar E-Type/6.jfif",
-];
-var index = 0;
-function changeImages2() {
-  imageSlideShow2.src = images2[index];
-  if (index < images2.length - 1) {
-    index++;
-  } else {
-    index = 0;
-  }
-  setTimeout("changeImages2()", 1200);
-}
-changeImages2();
 
 var images3 = [
   "img/Porsche-550-Spyder-with-Gold-Plated-Engine/1.jfif",
@@ -58,23 +66,3 @@ function changeImages3() {
   setTimeout("changeImages3()", 1200);
 }
 changeImages3();
-
-var images4 = [
-  "img/Fast And Furious Dodge Ice Charger/1.jfif",
-  "img/Fast And Furious Dodge Ice Charger/2.jfif",
-  "img/Fast And Furious Dodge Ice Charger/3.jfif",
-  "img/Fast And Furious Dodge Ice Charger/4.jfif",
-  "img/Fast And Furious Dodge Ice Charger/5.jfif",
-  "img/Fast And Furious Dodge Ice Charger/6.jfif",
-];
-var index = 0;
-function changeImages4() {
-  imageSlideShow4.src = images4[index];
-  if (index < images4.length - 1) {
-    index++;
-  } else {
-    index = 0;
-  }
-  setTimeout("changeImages4()", 1200);
-}
-changeImages4();
